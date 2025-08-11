81eec41bf39eed8179870d16be93f618cbb2f21a
ruhit3 <ruhit1011@gmail.com>
2025-08-11 17:16:43 +0600

    Resolve merge conflict

--- src/main/java/com/springcraft/se/Application.java
+++ src/main/java/com/springcraft/se/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraphqlDemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GraphqlDemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
